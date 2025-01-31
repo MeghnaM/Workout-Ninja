838e00efe533ececb872e8fc53ff5bf832bf4a8f
Meghna Malhotra <m_malhotra@protonmail.com>
2025-01-31 14:50:03 -0500

    change how I'm connecting to mongodb

--- server-and-db/index.js
+++ server-and-db/index.js
@@ -4,13 +4,14 @@
 const mongoose = require('mongoose');
 //const mongodbURI = 'mongodb://localhost:27017/workout-app-db';
 
-const username = encodeURIComponent("meghna");
-const password = encodeURIComponent("FRG8tgh*bte_any9xdu");
-const cluster = "workoutappcluster.1xt5j.mongodb.net";
-const appName = "WorkoutAppCluster";
-let mongodbURI =
-  `mongodb+srv://${username}:${password}@${cluster}/?retryWrites=true&w=majority&appName=${appName}`;
+// const username = encodeURIComponent("meghna");
+// const password = encodeURIComponent("FRG8tgh*bte_any9xdu");
+// const cluster = "workoutappcluster.1xt5j.mongodb.net";
+// const appName = "WorkoutAppCluster";
+// let mongodbURI =
+//   `mongodb+srv://${username}:${password}@${cluster}/?retryWrites=true&w=majority&appName=${appName}`;
 
+let mongodbURI = 'mongodb+srv://meghna:FRG8tgh*bte_any9xdu@workoutappcluster.1xt5j.mongodb.net/?retryWrites=true&w=majority&appName=WorkoutAppCluster';
 //const mongodbURI = 'mongodb+srv://meghna:@workoutappcluster.1xt5j.mongodb.net/?retryWrites=true&w=majority&appName=WorkoutAppCluster'
 const { Schema } = mongoose;
 
